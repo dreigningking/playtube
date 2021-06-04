aabf4532c049c0cd5d3c3e926ab7d332f868a139
reigningkingforever <reigningkingforever@gmail.com>
2021-06-04 02:40:24 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "playtube";
+
+// Site URL
+$site_url = "http://localhost/playtube"; // e.g (http://example.com)
+
+// Purchase code
+//$purchase_code = "12345678"; // Your purchase code, don't give it to anyone. 
+?>
